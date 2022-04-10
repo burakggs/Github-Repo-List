7c3bbf6b6fa8b7677196d8b2e21e5c98d927a3d8
burakggs <burak.gogus.27@gmail.com>
2022-04-11 01:18:50 +0300

    added repo detail screen

---
+++ app/src/main/java/com/burak/githubrepolist/RepoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.burak.githubrepolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RepoDetailActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
